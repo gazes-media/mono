33b29053fb6c3fe16f49fb13e621433965ff3234
exatombe <jeremy27.clara22@gmail.com>
2024-06-02 15:36:19 +0200

    Updating how update will work, script.ts will still reup everything.

--- apps/api/src/script.ts
+++ apps/api/src/script.ts
@@ -3,28 +3,17 @@ import { updateAnimeNeko, updateAnimeSama } from "@utils/updateAnime";
 import { createClient } from "redis";
 
 const prisma = new PrismaClient();
-const redis = createClient({
-    socket: {
-        host: "89.234.183.191",
-        port: 6379
-    }
-});
 
 async function main(prisma) {
     await updateAnimeNeko(prisma).then(async () => {
-        const animesToSave = await prisma.anime.findMany();
-        redis.set("animes", JSON.stringify(animesToSave));
         await updateAnimeSama(prisma).then(async () => {
-            const animeToUpdate = await prisma.anime.findMany();
-            redis.set("animes", JSON.stringify(animeToUpdate));
+            await prisma.anime.findMany();
         });
     });
 
     setInterval(async () => {
         await updateAnimeNeko(prisma).then(async () => {
             await updateAnimeSama(prisma).then(async () => {
-                const animeToUpdate = await prisma.anime.findMany();
-                redis.set("animes", JSON.stringify(animeToUpdate));
             });
         });
     }, 3600000)
