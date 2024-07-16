ffc8499233f6091f0a8e8845b3987080679d2fb9
l31 <l31@puffer.fish>
2024-07-17 00:29:13 +0200

    fix(animes): correct genre filtering logic and improve query handling
    - Fixed the logic for filtering genres to properly handle negations and inclusions.
    - Updated genre string to array conversion for proper processing.
    - Ensured year comparison includes converting `a.start_date_year` to a string.
    - Adjusted formatting for consistency and readability.
    
    This resolves issues where queries such as `/animes?genres=%20ecchi` were not working correctly.

--- apps/api/src/route/animes/Animes.route.ts
+++ apps/api/src/route/animes/Animes.route.ts
@@ -1,3 +1,4 @@
+
 import { HTTPMethods, RouteHandlerMethod } from "fastify";
 import Fuse from "fuse.js";
 import { Anime } from "../../interfaces/anime.interface";
@@ -5,87 +6,98 @@ import { AnimeStore } from "../../store/animes.store";
 import { Route } from "../Route";
 
 type AnimesQuery = {
-    types?: string;
-    status?: string;
-    genres?: string;
-    year?: string;
-    title?: string;
-    page?: number;
+  types?: string;
+  status?: string;
+  genres?: string;
+  year?: string;
+  title?: string;
+  page?: number;
 };
 
 export class AnimesRoute extends Route {
-    public url = "/animes";
-    public method: HTTPMethods = "GET";
+  public url = "/animes";
+  public method: HTTPMethods = "GET";
 
-    public handler: RouteHandlerMethod = (request, reply) => {
-        // récupérer les possible queries
-        const { types, status, genres, year, title, page }: AnimesQuery = Object.fromEntries(Object.entries(request.query).map(([key, value]) => [key, value?.toLowerCase()]));
+  public handler: RouteHandlerMethod = (request, reply) => {
+    // récupérer les possible queries
+    const { types, status, genres, year, title, page }: AnimesQuery = Object.fromEntries(Object.entries(request.query).map(([key, value]) => [key, value?.toLowerCase()]));
 
-        let animes = AnimeStore.vostfr;
+    let animes = AnimeStore.vostfr;
 
-        /**
-         * The function filters an array of anime objects based on various criteria such as type, language,
-         * status, genres, and years.
-         */
-        function animesFilter(a: Anime) {
-            let toreturn = true;
+    /**
+     * The function filters an array of anime objects based on various criteria such as type, language,
+     * status, genres, and years.
+     */
+    function animesFilter(a: Anime) {
+      let toreturn = true;
 
-            if (types && !types.split(",").includes(a.type.toString())) toreturn = false;
-            if (status && a.status !== status) toreturn = false;
+      if (types && !types.split(",").includes(a.type.toString())) toreturn = false;
+      if (status && a.status !== status) toreturn = false;
 
-            if (genres) {
-                let animeGenres = a.genres.map((g) => g.toLowerCase());
+      if (genres) {
+        let animeGenres = a.genres.map((g) => g.toLowerCase());
+        const genresArray = genres.split(",");
 
-                for (const genre of genres) {
-                    if (genre.startsWith("!") && !animeGenres.includes(genre.slice(0, 1))) toreturn = false;
-                    if (animeGenres.includes(genre)) toreturn = false;
-                }
+        for (const genre of genresArray) {
+          if (genre.startsWith("!")) {
+            if (animeGenres.includes(genre.substring(1))) {
+              toreturn = false;
+              break;
             }
-
-            if (year && !year.includes(a.start_date_year)) toreturn = false;
-            return toreturn;
+          } else {
+            if (!animeGenres.includes(genre)) {
+              toreturn = false;
+              break;
+            }
+          }
         }
+      }
 
-        animes = animes.filter(animesFilter);
+      if (year && !year.includes(a.start_date_year.toString())) toreturn = false;
+      return toreturn;
+    }
 
-        /**
-         * The function `titleFilter` takes an `Anime` object and a `title` string as input, and returns
-         * `true` if any of the titles in the `Anime` object (including English, French, Romanji, and
-         * others) contain the `title` string (case-insensitive), otherwise it returns `false`.
-         */
-        if (title) {
-            const fuse = new Fuse(animes, {
-                keys: ["title", "title_english", "title_romanji", "title_french"],
-                includeScore: false,
-            });
+    animes = animes.filter(animesFilter);
 
-            animes = fuse.search(title).map((a) => a.item);
-        }
+    /**
+     * The function `titleFilter` takes an `Anime` object and a `title` string as input, and returns
+     * `true` if any of the titles in the `Anime` object (including English, French, Romanji, and
+     * others) contain the `title` string (case-insensitive), otherwise it returns `false`.
+     */
+    if (title) {
+      const fuse = new Fuse(animes, {
+        keys: ["title", "title_english", "title_romanji", "title_french"],
+        includeScore: false,
+      });
 
-        if (animes.length <= 0) {
-            return reply.status(404).send({
-                success: false,
-                message: "La requête a été traitée avec succès, mais aucun contenu n'est disponible pour la réponse demandée.",
-            });
-        }
+      animes = fuse.search(title).map((a) => a.item);
+    }
 
-        if (page) {
-            const perPage = 20;
-            const start = (page - 1) * perPage;
-            const end = start + perPage;
+    if (animes.length <= 0) {
+      return reply.status(404).send({
+        success: false,
+        message: "La requête a été traitée avec succès, mais aucun contenu n'est disponible pour la réponse demandée.",
+      });
+    }
 
-            animes = animes.slice(start, end);
-        }
+    if (page) {
+      const perPage = 20;
+      const start = (page - 1) * perPage;
+      const end = start + perPage;
 
-        return reply.send({
-            success: true,
-            data: [...animes].map(({ url_image, coverUrl, url, id, nb_eps, title, ...anime }) => ({
-                id,
-                url_image,
-                url,
-                nb_eps,
-                title,
-            })),
-        });
-    };
+      animes = animes.slice(start, end);
+    }
+
+    return reply.send({
+      success: true,
+      data: [...animes].map(({ url_image, coverUrl, url, id, nb_eps, title, ...anime }) => ({
+        id,
+        url_image,
+        url,
+        nb_eps,
+        title,
+      })),
+    });
+  };
 }
+
